b15f87cb364927554489547358c3167ff6d2c80b
avisinghal6 <avisinghal6@gmail.com>
2023-07-16 18:58:06 -0700

    added css, structure for videoplayer,options,notifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import { Typography, AppBar } from '@material-ui/core';
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+import useStyles from './styles/AppStyles';
+
 
 function App() {
+  const classes= useStyles();
   return (
-    <div>
-      Video Call
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center"> Video Call</Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options> 
     </div>
     
   );
---
+++ client/src/styles/AppStyles.js
@@ -0,0 +1,26 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 100px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '600px',
+      border: '2px solid black',
+  
+      [theme.breakpoints.down('xs')]: {
+        width: '90%',
+      },
+    },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    },
+  }));
+
+  export default useStyles;
